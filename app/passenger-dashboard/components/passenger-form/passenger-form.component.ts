25889ddb746126ab9c7de0b8a9fb338c8ec4c1a8
mamarok <mamarok@gmail.com>
2018-04-28 19:59:01 +0200

    ngForm - ngModel

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -6,9 +6,26 @@ import {Passenger} from "../../models/passenger.interface";
     selector: 'passenger-form',
     styleUrls: ['passenger-form.component.scss'],
     template: `
-        <form>
+        <form #form="ngForm" novalidate>
             Form!!
             {{detail | json}}
+            <div>
+                Passenger name:
+                <input
+                        type="text"
+                        name="fullname"
+                        [ngModel]="detail?.fullname"
+                >
+            </div>
+            <div>
+                Passenger ID:
+                <input
+                        type="number"
+                        name="id"
+                        [ngModel]="detail?.id"
+                >
+            </div>
+            {{ form.value | json }}
         </form>
     `
 })
