d5d876ca1f7c7cc67f1bce1cf7dcf49784c7bede
Mateusz Czarnik <mamarok@gmail.com>
2018-04-29 13:30:43 +0200

    Feature-module routes with forChild()

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,20 +1,22 @@
 import {Component} from "@angular/core";
 
+interface Nav {
+    link: string,
+    name: string,
+    exact: boolean
+}
+
 @Component({
     selector: 'app-root',
     styleUrls: ['app.component.scss'],
     template: `
         <div class="app">
             <nav class="nav">
-                <a
-                        routerLink="/"
+                <a *ngFor="let item of nav"
+                        [routerLink]="item.link"
                         routerLinkActive="active"
-                        [routerLinkActiveOptions]="{ exact:true }"
-                >Home</a>
-                <a
-                        routerLink="/oops"
-                        routerLinkActive="active"
-                >404</a>
+                        [routerLinkActiveOptions]="{ exact: item.exact }"
+                >{{item.name}}</a>
             </nav>
             <router-outlet></router-outlet>
         </div>
@@ -22,5 +24,21 @@ import {Component} from "@angular/core";
 })
 
 export class AppComponent {
-
+    nav: Nav[] = [
+        {
+            link: '/',
+            name: 'Home',
+            exact: true
+        },
+        {
+            link: '/passengers',
+            name: 'Passengers',
+            exact: true
+        },
+        {
+            link: '/oops',
+            name: 'Oops',
+            exact: false
+        }
+    ]
 }
--- app/passenger-dashboard/passenger-dashboard-module.ts
+++ app/passenger-dashboard/passenger-dashboard-module.ts
@@ -1,7 +1,8 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {HttpModule} from "@angular/http";
-import { FormsModule} from "@angular/forms";
+import {FormsModule} from "@angular/forms";
+import {RouterModule, Routes} from "@angular/router";
 
 // container
 import {PassengerDashboardComponent} from "./containers/passenger-dashboard/passenger-dashboard.component";
@@ -14,6 +15,13 @@ import {PassengerFormComponent} from "./components/passenger-form/passenger-form
 // services
 import {PassengerDashboardService} from "./passenger-dashboard.service";
 
+const routes: Routes = [
+    {
+        path: 'passengers',
+        component: PassengerDashboardComponent
+    }
+];
+
 @NgModule({
     declarations: [
         PassengerDashboardComponent,
@@ -25,10 +33,8 @@ import {PassengerDashboardService} from "./passenger-dashboard.service";
     imports: [
         CommonModule,
         HttpModule,
-        FormsModule
-    ],
-    exports: [
-        PassengerViewerComponent
+        FormsModule,
+        RouterModule.forChild(routes)
     ],
     providers: [
         PassengerDashboardService
